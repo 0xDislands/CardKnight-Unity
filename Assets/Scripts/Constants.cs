57ba7487b567718a47b316d99f9a1f85472ba5f5
Huynh Duc Huy <duchuy.huynh.413@gmail.com>
2024-09-07 23:18:44 +0700

    update

--- Assets/Scripts/Constants.cs
+++ Assets/Scripts/Constants.cs
@@ -1,7 +1,7 @@
 ﻿public class Constants
 {
     public static bool SHOW_CHEAT_OBJECT = false;
-    public static bool CHEAT_NO_GAME_OVER = true;
+    public static bool CHEAT_NO_GAME_OVER = false;
     public static bool SHOW_DEBUG = false;
     public static bool TEST_SKILL_FIRE = false;
 }
