118c9a89b77344c4db9a76bfdb76d46668217b4e
Duchuy <duchuy.huynh.413@gmail.com>
2024-09-11 23:12:33 +0700

    update popup chest mini game

--- Assets/Scripts/Battle/ItemChestMiniGame.cs
+++ Assets/Scripts/Battle/ItemChestMiniGame.cs
@@ -6,11 +6,32 @@ using DG.Tweening;
 public class ItemChestMiniGame : Item
 {
     public ParticleSystem appearEffect;
-    public List<CardId> transformCards = new List<CardId>() { CardId.ItemHeal, CardId.ItemPoison, CardId.ItemShield };
     public override void ApplyEffect(Hero hero)
     {
         CardManager.Instance.hero.hasMove = false;
         var card = GetComponent<Card>();
-        Gameplay.Instance.popupUnlockMiniGame.ShowMiniGame(card, transformCards.RandomElement());
+        Gameplay.Instance.popupUnlockMiniGame.ShowMiniGame(card, GetRandomCard());
+    }
+    public CardId GetRandomCard()
+    {
+        int ratio1 = 15;
+        int ratio2 = 30 + ratio1;
+        int ratio3 = 10 + ratio2;
+        int ratio4 = 55 + ratio3;
+
+        int rand = Random.Range(0, 101);
+        if (rand < ratio1)
+        {
+            return CardId.SkillFire;
+        } else if (rand < ratio2)
+        {
+            return CardId.ItemShield;
+        } else if (rand < ratio3)
+        {
+            return CardId.ItemPoison;
+        } else
+        {
+            return CardId.ItemHeal;
+        }
     }
 }
