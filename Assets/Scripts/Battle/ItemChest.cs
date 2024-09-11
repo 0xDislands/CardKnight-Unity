e8d188066e1e66d084c0c296813d791751b193b8
Duchuy <duchuy.huynh.413@gmail.com>
2024-09-11 22:59:52 +0700

    update

--- Assets/Scripts/Battle/ItemChest.cs
+++ Assets/Scripts/Battle/ItemChest.cs
@@ -5,15 +5,38 @@ using System.Collections.Generic;
 public class ItemChest : Item
 {
     public ParticleSystem appearEffect;
-    public List<CardId> transformCards = new List<CardId>() { CardId.ItemHeal, CardId.ItemPoison, CardId.ItemShield };
     public override void ApplyEffect(Hero hero)
     {
         CardManager.Instance.hero.hasMove = false;
         var effect = SimpleObjectPool.Instance.GetObjectFromPool(appearEffect, transform.position);
         var card = GetComponent<Card>();
         Debug.Log("calling spawn card item chest");
-        var newCard = CardManager.Instance.SpawnCard(card.Pos, transformCards.RandomElement());
+        var newCard = CardManager.Instance.SpawnCard(card.Pos, GetRandomCard());
         newCard.MoveToPos(card.Pos);
         card.Disappear();
+    }
+
+    public CardId GetRandomCard()
+    {
+        int ratio1 = 5;
+        int ratio2 = 30 + ratio1;
+        int ratio3 = 10 + ratio2;
+        int ratio4 = 55 + ratio3;
+
+        int rand = Random.Range(0, 101);
+        if (rand < ratio1)
+        {
+            return CardId.SkillFire;
+        } 
+        else if (rand < ratio2)
+        {
+            return CardId.ItemShield;
+        } else if (rand < ratio3)
+        {
+            return CardId.ItemPoison;
+        } else
+        {
+            return CardId.ItemHeal;
+        }
     }
 }
